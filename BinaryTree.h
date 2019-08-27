b7ae30cc7ea715ee57d39c244ed29eb637c3c2bb
dotruger <dotruger.37@gmail.com>
2019-08-27 23:30:14 +0300

    main ControlCenter.cpp added
    For output menu binary tree

---
+++ BinaryTree.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <iostream>
+
+typedef struct binary_tree tree_s;
+
+struct binary_tree {
+	int value;
+	tree_s* left, * right;
+
+	binary_tree(tree_s* new_left, tree_s* new_right, int new_value) {
+		left = new_left;
+		right = new_right;
+		value = new_value;
+	}
+};
+
+void add_node(tree_s*& node, int val);
+void visual_tree(tree_s*& p, int level);
+void print_tree(tree_s*& node);
+tree_s* find_node(tree_s*& node, int value);
+void free_tree(tree_s* node);
+
+void actions_binarytree();
