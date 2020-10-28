a5ff40c4a0455c0f702b2b72d30348668303b735
Timonin Anton <dotruger.37@gmail.com>
2020-10-28 10:06:33 +0300

    Add completed version

--- module_1/task_2_2/main.cpp
+++ module_1/task_2_2/main.cpp
@@ -17,26 +17,27 @@ int binary_search(int* array, int l, int r) {
     while (left <= right) {
         int mid = left + (right - left) / 2;
         
-        if (array[mid] > array[mid+1]) {
-            return array[mid+1];
-        } else if (array[mid] < array[mid+1]) {
+        if (array[mid] < array[mid+1]) {
             left = mid + 1;
+        } else if (array[mid] > array[mid+1]) {
+            right = mid - 1;
+        } else {
+            left += 1;
         }
     }
-    return array[left];
+    return left;
 }
 
 int search(int* array, int size) {
-    if (size == 1) return array[0];
-    
-    
     int i = 1;
     
-    while(i < (size - 1) && array[i-1] < array[i]) {
+    while(i < (size - 1) && array[i-1] <= array[i]) {
+        int arri = array[i];
+        int arri1 = array[i-1];
         i *= 2;
     }
     
-    int num = binary_search(array, i/2, i > (size) ? (size): i);
+    int num = binary_search(array, i/2, i > (size) ? (size-1): i);
     return num;
 }
 
@@ -47,7 +48,7 @@ void testLogic() {
         sstr_input << "1 2 -1" << std::endl;
         std::stringstream sstr_output;
         run(sstr_input, sstr_output);
-        assert(sstr_output.str() == "-1\n");
+        assert(sstr_output.str() == "1\n");
     }
     {
         std::stringstream sstr_input;
@@ -55,7 +56,7 @@ void testLogic() {
         sstr_input << "1 -100 -101 -102 -103" << std::endl;
         std::stringstream sstr_output;
         run(sstr_input, sstr_output);
-        assert(sstr_output.str() == "-100\n");
+        assert(sstr_output.str() == "0\n");
     }
     {
         std::stringstream sstr_input;
@@ -63,43 +64,171 @@ void testLogic() {
         sstr_input << "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 -1 -2 -3 -4 -5 -6 -7 -8 -9 -10 -11 -12 -13" << std::endl;
         std::stringstream sstr_output;
         run(sstr_input, sstr_output);
-        assert(sstr_output.str() == "-1\n");
+        assert(sstr_output.str() == "14\n");
     }
-    
+
     {
         std::stringstream sstr_input;
         sstr_input << "1" << std::endl;
         sstr_input << "10" << std::endl;
         std::stringstream sstr_output;
         run(sstr_input, sstr_output);
-        assert(sstr_output.str() == "10\n");
+        assert(sstr_output.str() == "0\n");
     }
-    
+
     {
         std::stringstream sstr_input;
         sstr_input << "2" << std::endl;
         sstr_input << "1 2" << std::endl;
         std::stringstream sstr_output;
         run(sstr_input, sstr_output);
-        assert(sstr_output.str() == "2\n");
+        assert(sstr_output.str() == "1\n");
     }
-    
+
     {
         std::stringstream sstr_input;
         sstr_input << "2" << std::endl;
         sstr_input << "1 -1" << std::endl;
         std::stringstream sstr_output;
         run(sstr_input, sstr_output);
-        assert(sstr_output.str() == "-1\n");
+        assert(sstr_output.str() == "0\n");
     }
-    
+    {
+        std::stringstream sstr_input;
+        sstr_input << "3" << std::endl;
+        sstr_input << "1 2 3" << std::endl;
+        std::stringstream sstr_output;
+        run(sstr_input, sstr_output);
+        assert(sstr_output.str() == "2\n");
+    }
+    {
+        std::stringstream sstr_input;
+        sstr_input << "1" << std::endl;
+        sstr_input << "5" << std::endl;
+        std::stringstream sstr_output;
+        run(sstr_input, sstr_output);
+        assert(sstr_output.str() == "0\n");
+    }
+    {
+        std::stringstream sstr_input;
+        sstr_input << "5" << std::endl;
+        sstr_input << "-1 -2 -3 -4 -5" << std::endl;
+        std::stringstream sstr_output;
+        run(sstr_input, sstr_output);
+        assert(sstr_output.str() == "0\n");
+    }
+    {
+        std::stringstream sstr_input;
+        sstr_input << "4" << std::endl;
+        sstr_input << "1 2 -3 -4" << std::endl;
+        std::stringstream sstr_output;
+        run(sstr_input, sstr_output);
+        assert(sstr_output.str() == "1\n");
+    }
+    {
+        std::stringstream sstr_input;
+        sstr_input << "4" << std::endl;
+        sstr_input << "1 -2 -3 -4" << std::endl;
+        std::stringstream sstr_output;
+        run(sstr_input, sstr_output);
+        assert(sstr_output.str() == "0\n");
+    }
+    {
+        std::stringstream sstr_input;
+        sstr_input << "4" << std::endl;
+        sstr_input << "1 2 3 -1" << std::endl;
+        std::stringstream sstr_output;
+        run(sstr_input, sstr_output);
+        assert(sstr_output.str() == "2\n");
+    }
+    {
+        std::stringstream sstr_input;
+        sstr_input << "5" << std::endl;
+        sstr_input << "1 2 3 4 -1" << std::endl;
+        std::stringstream sstr_output;
+        run(sstr_input, sstr_output);
+        assert(sstr_output.str() == "3\n");
+    }
+    {
+        std::stringstream sstr_input;
+        sstr_input << "6" << std::endl;
+        sstr_input << "1 2 3 4 -1 -2" << std::endl;
+        std::stringstream sstr_output;
+        run(sstr_input, sstr_output);
+        assert(sstr_output.str() == "3\n");
+    }
+    {
+        std::stringstream sstr_input;
+        sstr_input << "6" << std::endl;
+        sstr_input << "1 2 3 0 -1 -2" << std::endl;
+        std::stringstream sstr_output;
+        run(sstr_input, sstr_output);
+        assert(sstr_output.str() == "2\n");
+    }
     {
         std::stringstream sstr_input;
         sstr_input << "2" << std::endl;
         sstr_input << "-1 -2" << std::endl;
         std::stringstream sstr_output;
         run(sstr_input, sstr_output);
-        assert(sstr_output.str() == "-2\n");
+        assert(sstr_output.str() == "0\n");
+    }
+    {
+        std::stringstream sstr_input;
+        sstr_input << "3" << std::endl;
+        sstr_input << "-1 -2 -3" << std::endl;
+        std::stringstream sstr_output;
+        run(sstr_input, sstr_output);
+        assert(sstr_output.str() == "0\n");
+    }
+    {
+        std::stringstream sstr_input;
+        sstr_input << "5" << std::endl;
+        sstr_input << "-1 -2 -3 -4 -5" << std::endl;
+        std::stringstream sstr_output;
+        run(sstr_input, sstr_output);
+        assert(sstr_output.str() == "0\n");
+    }
+    {
+        std::stringstream sstr_input;
+        sstr_input << "6" << std::endl;
+        sstr_input << "1 2 -1 -2 -3 -4" << std::endl;
+        std::stringstream sstr_output;
+        run(sstr_input, sstr_output);
+        assert(sstr_output.str() == "1\n");
+    }
+    {
+        std::stringstream sstr_input;
+        sstr_input << "6" << std::endl;
+        sstr_input << "1 0 -1 -2 -3 -4" << std::endl;
+        std::stringstream sstr_output;
+        run(sstr_input, sstr_output);
+        assert(sstr_output.str() == "0\n");
+    }
+    {
+        std::stringstream sstr_input;
+        sstr_input << "6" << std::endl;
+        sstr_input << "1 2 3 4 5 -1" << std::endl;
+        std::stringstream sstr_output;
+        run(sstr_input, sstr_output);
+        assert(sstr_output.str() == "4\n");
+    }
+    {
+        std::stringstream sstr_input;
+        sstr_input << "6" << std::endl;
+        sstr_input << "1 2 3 4 -1 -2" << std::endl;
+        std::stringstream sstr_output;
+        run(sstr_input, sstr_output);
+        assert(sstr_output.str() == "3\n");
+    }
+
+    {
+        std::stringstream sstr_input;
+        sstr_input << "6" << std::endl;
+        sstr_input << "1 2 3 4 5 6" << std::endl;
+        std::stringstream sstr_output;
+        run(sstr_input, sstr_output);
+        assert(sstr_output.str() == "5\n");
     }
 }
 
@@ -125,9 +254,13 @@ int run(std::istream& input, std::ostream& output) {
 }
 
 int main(int argc, const char * argv[]) {
-    
-    //testLogic();
-    //std::cout << "OK" << std::endl;
-    
+//    testLogic();
+//    std::cout << "OK" << std::endl;
+//
     return run(std::cin, std::cout);
+    
+//    std::stringstream sstr_input;
+//    sstr_input << "6" << std::endl;
+//    sstr_input << "1 2 3 4 5 6" << std::endl;
+//    return run(sstr_input, std::cout);
 }
