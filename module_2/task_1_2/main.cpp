343eb46187a125b58a55f49960827ace014deb5c
Anton Timonin <dotruger.37@gmail.com>
2020-11-27 10:16:30 +0300

    Update main.cpp

--- module_2/task_1_2/main.cpp
+++ module_2/task_1_2/main.cpp
@@ -6,20 +6,24 @@
 #include <vector>
 #include <assert.h>
 
+#define NUM1 79
+#define NUM2 101
+#define NUM3 857
+
 using std::string;
 
 template<class T> struct HashFunc;
 
 template<> struct HashFunc<int> {
-    size_t operator() (const int& key) {
+    size_t operator() (const int& key, const int& simpleNum) {
         return key;
     }
 };
 
 template<> struct HashFunc<std::string> {
-    size_t operator() (const std::string& key) {
+    size_t operator() (const std::string& key, const int& simpleNum) {
         size_t hash = 0;
-        int a = 37;
+        int a = simpleNum;
         for(int i = 0; i < key.size(); i++) {
             hash = (hash * a + key[i]);
         }
@@ -41,13 +45,15 @@ struct Node {
     //  1 - not free
     int status;
 
-    void set(const Key& key, const Value& value, int8_t status) {
+    void set(const Key& key, const Value& value, int status) {
         this->key = key;
         this->value = value;
         this->status = status;
     }
     
     Node() {
+        key = "";
+        value = "";
         status = 0;
     }
 };
@@ -55,14 +61,20 @@ struct Node {
 template <class Key, class Value, class Hash = HashFunc<Key>>
 class HashTable {
 public:
-    explicit HashTable(Hash hash = Hash()): buckets(0), buckets_count(0), index_idx(-1), items_count(0), hash(hash) { }
+    explicit HashTable(Hash hash = Hash()): buckets(0), items_count(0), hash(hash) {
+        buckets_count = 11;
+        for (int i = 0; i < buckets_count; i++) {
+            buckets.push_back(Node<Key, Value>());
+        }
+    }
     
     ~HashTable() {
         buckets.clear();
     }
 
     bool insert(const Key& key, const Value& value) {
-        if (!buckets.size() || 3 * buckets_count < 4 * items_count) {
+//        std::cout << 3 * buckets_count << " < " << 4 * items_count << std::endl;
+        if (!buckets.size() || (3 * buckets_count) <  (4 * items_count)) {
             grow();
         }
         
@@ -77,9 +89,10 @@ public:
                 break;
             }
         }
-
+        
 //        std::cout << "\ninsert key = " << key << "; value = " << value << std::endl;
 //        std::cout << "items_count = " << items_count << std::endl;
+//        print();
         return true;
     }
 
@@ -90,10 +103,16 @@ public:
             if (buckets[index].status == 1 && buckets[index].key == key) {
                 return true;
             } else if (buckets[index].status == 0) {
-                break;
+                return false;
             }
         }
         return false;
+    }
+    
+    void print() {
+        for (int i = 0; i < buckets.size(); i++) {
+            std::cout << i << " - " << buckets[i].key << std::endl;
+        }
     }
 
     bool remove(const Key& key) {
@@ -105,7 +124,7 @@ public:
                 items_count--;
                 return true;
             } else if (buckets[index].status == 0) {
-                break;
+                return false;
             }
         }
         return false;
@@ -116,18 +135,20 @@ private:
     size_t buckets_count;
     size_t items_count;
 
-    size_t index_idx;
-
     Hash hash;
 
     void grow() {
         std::vector<Node<Key, Value>> old_buckets = buckets;
-        buckets_count = BUCKETS_COUNT[++index_idx];
-        buckets = std::vector<Node<Key, Value>>(buckets_count);
+        buckets_count *= 2;
+        buckets = std::vector<Node<Key, Value>>(0);
         items_count = 0;
         
 //        std::cout << "buckets_count = " << buckets_count << std::endl;
 //        std::cout << "items_count = " << items_count << std::endl;
+        
+        for (int i = 0; i < buckets_count; i++) {
+            buckets.push_back(Node<Key, Value>());
+        }
         
         for (int i = 0; i < old_buckets.size(); i++) {
             if (old_buckets[i].status == 1) {
@@ -141,10 +162,10 @@ private:
     }
 
     size_t hash_double(const Key& key, int iteration) {
-        size_t size = BUCKETS_COUNT[index_idx];
-        size_t hash_value1 = hash(key) % size;
-        size_t hash_value2 = (hash(key) + 1) % size;
-        return (hash_value1 + hash_value2 * (1 + iteration)) % size;
+        size_t size = buckets_count;
+        size_t hash_value1 = hash(key, NUM1);
+        size_t hash_value2 = 2 * hash(key, NUM2) + 1;
+        return (hash_value1 + hash_value2 * iteration) % size;
     }
 };
 
@@ -176,6 +197,8 @@ int run(std::istream& input, std::ostream& output) {
             }
         }
     
+//    hash.print();
+    
     return 0;
 }
 
@@ -183,12 +206,12 @@ void testLogic() {
     {
         std::stringstream sstr_input;
         std::string operation = "+";
-        for (int i = 0; i < 70000; i++) {
+        for (int i = 0; i < 10000; i++) {
             sstr_input << operation << " "<< std::to_string(i) << std::endl;
         }
-        
+
         operation = "-";
-        for (int i = 0; i < 70000; i++) {
+        for (int i = 0; i < 10000; i++) {
             sstr_input << "?" << " " << std::to_string(i) << std::endl;
             sstr_input << operation << " "<< std::to_string(i) << std::endl;
             sstr_input << "?" << " " << std::to_string(i) << std::endl;
@@ -196,10 +219,86 @@ void testLogic() {
         std::stringstream sstr_output;
         run(sstr_input, sstr_output);
 //        assert(sstr_output.str() == "1\n");
+//        std::cout << sstr_output.str() << std::endl;
+    }
+    {
+        std::stringstream sstr_input;
+        for (int i = 0; i < 2; i++) {
+            sstr_input << "?" << " "<< std::to_string(i) << std::endl;
+            sstr_input << "+" << " "<< std::to_string(i) << std::endl;
+            sstr_input << "?" << " "<< std::to_string(i) << std::endl;
+        }
+
+        for (int i = 0; i < 2; i++) {
+            sstr_input << "-" << " "<< std::to_string(i) << std::endl;
+            sstr_input << "?" << " "<< std::to_string(i) << std::endl;
+
+        }
+        std::stringstream sstr_output;
+        run(sstr_input, sstr_output);
+        assert(sstr_output.str() == "FAIL\nOK\nOK\nFAIL\nOK\nOK\nOK\nFAIL\nOK\nFAIL\n");
+        std::cout << sstr_output.str() << std::endl;
+    }
+    {
+        std::stringstream sstr_input;
+        sstr_input << "?" << " " << "123" << std::endl;
+        sstr_input << "+" << " " << "123" << std::endl;
+        sstr_input << "+" << " " << "123" << std::endl;
+        sstr_input << "?" << " " << "123" << std::endl;
+        sstr_input << "-" << " " << "123" << std::endl;
+        sstr_input << "?" << " " << "123" << std::endl;
+        std::stringstream sstr_output;
+        run(sstr_input, sstr_output);
+        assert(sstr_output.str() == "FAIL\nOK\nFAIL\nOK\nOK\nFAIL\n");
+        std::cout << sstr_output.str() << std::endl;
+    }
+    {
+        std::stringstream sstr_input;
+        sstr_input << "+" << " " << "a" << std::endl;
+        sstr_input << "+" << " " << "ab" << std::endl;
+        sstr_input << "+" << " " << "abc" << std::endl;
+        sstr_input << "+" << " " << "abc1" << std::endl;
+        sstr_input << "+" << " " << "abc2" << std::endl;
+
+        sstr_input << "-" << " " << "a" << std::endl;
+
+        sstr_input << "+" << " " << "abcd" << std::endl;
+
+        sstr_input << "-" << " " << "ab" << std::endl;
+
+        sstr_input << "+" << " " << "abcde" << std::endl;
+        sstr_input << "+" << " " << "abcdef" << std::endl;
+
+        sstr_input << "?" << " " << "a" << std::endl;
+        sstr_input << "+" << " " << "a" << std::endl;
+        sstr_input << "?" << " " << "a" << std::endl;
+        sstr_input << "+" << " " << "a" << std::endl;
+
+        std::stringstream sstr_output;
+        run(sstr_input, sstr_output);
+        std::cout << sstr_output.str() << std::endl;
+        assert(sstr_output.str() == "OK\nOK\nOK\nOK\nOK\nOK\nOK\nOK\nOK\nOK\nFAIL\nOK\nOK\nFAIL\n");
+        std::cout << sstr_output.str() << std::endl;
+    }
+    {
+        std::stringstream sstr_input;
+
+        sstr_input << "+" << " " << "%%%%%%" << std::endl;
+        sstr_input << "+" << " " << "JJJ" << std::endl;
+        sstr_input << "+" << " " << "%%JJ" << std::endl;
+        sstr_input << "+" << " " << "J%%J" << std::endl;
+        sstr_input << "?" << " " << "J%%J" << std::endl;
+        sstr_input << "+" << " " << "JJ%%" << std::endl;
+        sstr_input << "+" << " " << "J%%%%" << std::endl;
+        sstr_input << "+" << " " << "%%%%J" << std::endl;
+
+        std::stringstream sstr_output;
+        run(sstr_input, sstr_output);
+        std::cout << sstr_output.str() << std::endl;
+        assert(sstr_output.str() == "OK\nOK\nOK\nOK\nOK\nOK\nOK\nOK\n");
         std::cout << sstr_output.str() << std::endl;
     }
 }
-
 
 int main(int argc, const char * argv[]) {
 //    testLogic();
@@ -208,4 +307,3 @@ int main(int argc, const char * argv[]) {
     
     return run(std::cin, std::cout);
 }
-
