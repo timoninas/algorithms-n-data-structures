3615c03f4218d90160daa879df7f0e6e648f62ac
Anton Timonin <dotruger.37@gmail.com>
2020-11-29 12:49:12 +0300

    add base methods without avl helper functions

--- module_2/task_4_2/main.cpp
+++ module_2/task_4_2/main.cpp
@@ -35,7 +35,15 @@ public:
     }
     
     Value* find(const Key& key) {
-        return find_aux(key, root);
+        return find_aux(root, key);
+    }
+    
+    void insert(const Key& key, const Value& value) {
+        root = insert_aux(root, key, value);
+    }
+    
+    void erase(const Key& key) {
+        root = erase_aux(root, key);
     }
     
 private:
@@ -44,7 +52,7 @@ private:
     
     Comparator comp;
     
-    Value* find_aux(const Key& key, Node* node) {
+    Value* find_aux(Node* node, const Key& key) {
         if (!node) {
             return nullptr;
         }
@@ -56,6 +64,78 @@ private:
             return find_aux(key, node->right);
         }
         return &node->value;
+    }
+    
+    Node* insert_aux(Node* node, const Key& key, const Value& value) {
+        if (!node) {
+            node_count++;
+            return new Node(key, value);
+        }
+        
+        int res = comp(key, node->key);
+        if (res == -1) {
+            node->left =  find_aux(key, node->left);
+        } else if (res == 1) {
+            node->right =  find_aux(key, node->right);
+        }
+        return balance(node);
+    }
+    
+    Node* erase_aux(Node* node, const Key& key) {
+        if (!node) {
+            return nullptr;
+        }
+        
+        int res = comp(key, node->key);
+        if (res == -1) {
+            node->left =  find_aux(key, node->left);
+        } else if (res == 1) {
+            node->right =  find_aux(key, node->right);
+        } else {
+            Node* left = node->left;
+            Node* right = node->right;
+            
+            delete node;
+            
+            if (node->right == nullptr) {
+                return left;
+            }
+            
+            Node* min_node = find_and_remove_min(right);
+            min_node->right = right;
+            min_node->left = left;
+            
+            return balance(min_node);
+        }
+        
+        return balance(node);
+    }
+    
+    Node* find_and_remove_min(Node* node) {
+        Node* curNode = node;
+        while(curNode->left) {
+            curNode = curNode->left;
+        }
+        
+        Node* finded_min = curNode; // 5
+        
+        curNode = node;
+        while(curNode->left) {
+            if (curNode->left->key == finded_min->key) {
+                break;
+            }
+            curNode = curNode->left;
+        }
+        
+        curNode->left = finded_min->right;
+        finded_min->left = nullptr;
+        finded_min->right = nullptr;
+        
+        return finded_min;
+    }
+    
+    Node* balance(Node* node) {
+        return nullptr;
     }
 };
 
