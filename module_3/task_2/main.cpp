7920534dc140521a67d46b42377e7eee13d6e317
Timonin Anton <dotruger.37@gmail.com>
2020-12-26 14:19:05 +0500

    Fix AddEdge in graph

--- module_3/task_2/main.cpp
+++ module_3/task_2/main.cpp
@@ -5,6 +5,8 @@
 #include <vector>
 #include <queue>
 
+#include <assert.h>
+
 // Дан невзвешенный неориентированный граф. В графе может быть несколько
 // кратчайших путей между какими-то вершинами. Найдите количество
 // различных кратчайших путей между заданными вершинами
@@ -38,16 +40,17 @@ public:
     
     virtual ~ListGraph() {}
     
-    int VerticesCount() const {
+    int VerticesCount() const override {
         return graph.size();
     }
     
-    void AddEdge(int from, int to) {
+    void AddEdge(int from, int to) override {
         assert(from < graph.size());
         assert(to < graph.size());
         assert(from >= 0);
         assert(to >= 0);
         graph[from].push_back(to);
+        graph[to].push_back(from);
     }
     
     std::vector<int> GetNextVertices(int vertex) const override {
@@ -85,35 +88,35 @@ int bfs(const IGraph& graph, int from, int to) {
     visited.resize(graph.VerticesCount(), false);
     
     std::vector<int> routes;
-    routes.resize(graph.VerticesCount(), INT_MAX);
+    routes.resize(graph.VerticesCount(), graph.VerticesCount());
     
-    std::vector<int> min_routes;
-    min_routes.resize(graph.VerticesCount(), 0);
+    std::vector<int> min_path_count;
+    min_path_count.resize(graph.VerticesCount(), 0);
     
     queue.push(from);
     visited[from] = true;
     routes[from] = 0;
-    min_routes[from] = 1;
+    min_path_count[from] = 1;
     
     while (!queue.empty()) {
         int vertex = queue.front(); queue.pop();
         
         for (auto child: graph.GetNextVertices(vertex)) {
             if (routes[child] > routes[vertex] + 1) {
-                min_routes[child] = min_routes[vertex];
+                min_path_count[child] = min_path_count[vertex];
                 routes[child] = routes[vertex] + 1;
             } else if (routes[child] == routes[vertex] + 1) {
-                min_routes[child] += min_routes[vertex];
+                min_path_count[child] += min_path_count[vertex];
             }
             
             if (!visited[child]) {
-                queue.push(child);
                 visited[child] = true;
+                queue.push(child);
             }
         }
     }
     
-    return min_routes[to];
+    return min_path_count[to];
 }
 
 int run(std::istream& input, std::ostream& output);
@@ -132,7 +135,32 @@ void testLogic() {
         sstr_input << "0 3" << std::endl;
         std::stringstream sstr_output;
         run(sstr_input, sstr_output);
-        assert(sstr_output.str() == "2\n");
+        assert(sstr_output.str() == "2");
+    }
+    {
+        std::stringstream sstr_input;
+        sstr_input << "4" << std::endl;
+        sstr_input << "5" << std::endl;
+        sstr_input << "0 1" << std::endl;
+        sstr_input << "0 2" << std::endl;
+        sstr_input << "1 2" << std::endl;
+        sstr_input << "1 3" << std::endl;
+        sstr_input << "2 3" << std::endl;
+        
+        sstr_input << "0 2" << std::endl;
+        std::stringstream sstr_output;
+        run(sstr_input, sstr_output);
+        assert(sstr_output.str() == "1");
+    }
+    {
+        std::stringstream sstr_input;
+        sstr_input << "2" << std::endl;
+        sstr_input << "0" << std::endl;
+        
+        sstr_input << "0 1" << std::endl;
+        std::stringstream sstr_output;
+        run(sstr_input, sstr_output);
+        assert(sstr_output.str() == "0");
     }
     {
         std::stringstream sstr_input;
@@ -150,13 +178,37 @@ void testLogic() {
         sstr_input << "0 6" << std::endl;
         std::stringstream sstr_output;
         run(sstr_input, sstr_output);
-        assert(sstr_output.str() == "4\n");
+        assert(sstr_output.str() == "4");
+    }
+    {
+        std::stringstream sstr_input;
+        sstr_input << "7" << std::endl;
+        sstr_input << "11" << std::endl;
+        sstr_input << "0 1" << std::endl;
+        sstr_input << "0 2" << std::endl;
+        sstr_input << "0 3" << std::endl;
+        sstr_input << "1 2" << std::endl;
+        sstr_input << "2 3" << std::endl;
+        sstr_input << "1 4" << std::endl;
+        sstr_input << "2 4" << std::endl;
+        sstr_input << "2 5" << std::endl;
+        sstr_input << "3 5" << std::endl;
+        sstr_input << "4 6" << std::endl;
+        sstr_input << "5 6" << std::endl;
+        
+        sstr_input << "0 6" << std::endl;
+        std::stringstream sstr_output;
+        run(sstr_input, sstr_output);
+        assert(sstr_output.str() == "4");
     }
 }
 
 int run(std::istream& input, std::ostream& output) {
     int v, n;
     input >> v >> n;
+    
+    assert(v <= 50000);
+    assert(n <= 200000);
     
     ListGraph graph(v);
     
@@ -169,15 +221,15 @@ int run(std::istream& input, std::ostream& output) {
     int from, to;
     input >> from >> to;
     
-    output << bfs(graph, from, to) << "\n";
+    output << bfs(graph, from, to);
     
     return 0;
 }
 
 int main(int argc, const char * argv[]) {
-    testLogic();
-    std::cout << "TEST PASSED" << std::endl;
-    return 0;
+//    testLogic();
+//    std::cout << "TEST PASSED" << std::endl;
+//    return 0;
     
-//    return run(std::cin, std::cout);
+    return run(std::cin, std::cout);
 }
